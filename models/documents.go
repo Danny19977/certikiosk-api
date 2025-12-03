a02a2c1bfae92782eab12ae79c9a1c5fdac23900
what-markeart <what-markeart@gmail.com>
2025-12-03 12:09:38 +0100

    fix: Update DocumentDataUrl field to correct JSON tag and maintain GORM mapping

--- models/documents.go
+++ models/documents.go
@@ -7,7 +7,7 @@ type Documents struct {
 
 	NationalID      int64     `json:"national_id"`
 	DocumentType    string    `json:"document_type"`
-	DocumentDataUrl string    `json:"document_data_url"`
+	DocumentDataUrl string    `json:"document_data" gorm:"column:document_data_url"`
 	UserUUID        string    `json:"user_uuid"`
 	IssueDate       time.Time `json:"issue_date"`
 	IsActive        bool      `json:"is_active"`
